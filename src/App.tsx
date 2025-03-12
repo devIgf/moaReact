9c99d23ef0e94c5ddbfc144482965cef8da45505
devIgf <igfdev0@gmail.com>
2025-03-12 16:02:12 +0000

    fin des 1h20 des bases de React

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = [
-    "New York",
-    "San Francisco",
-    "Tokyo",
-    "London",
-    "Paris",
-    "Libreville",
-  ];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
+  const [alertVisible, setAlerteVisibility] = useState(false);
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelecItem={handleSelectItem}
-      />
+      {alertVisible && (
+        <Alert onClose={() => setAlerteVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlerteVisibility(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: string;
+  color?: string;
+  onClick: () => void;
+}
+
+export const Button = ({ children, color = "primary", onClick }: Props) => {
+  return (
+    <button className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
